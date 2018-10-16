08db50337808061d3280391c6079a23fb6509ece
sbrink00 <sbrink00@stuy.edu>
2018-10-15 20:48:53 -0400

    wrote an outline for both classes by declaring instance variables, constructors, and methods, and filling those with dummy values

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,3 +1,32 @@
 public class RationalNumber extends RealNumber{
-  
+  private int numerator;
+  private int denominator;
+
+  public RationalNumber(int nume, int denom){
+    numerator = nume; //this constructor will deal with the issue of
+    denominator = denom; //zero as a denominator
+  }
+
+  public RationalNumber(){
+    numerator = 1;
+    denominator = 1;
+  }
+
+  public String toString(){
+    return "";
+  }
+
+  public void setNum(int nume, int denom){
+    numerator = nume;
+    denominator = denom;
+  }
+
+  public String compareTo(RationalNumber other){
+    return "the return statement will compare the RationalNumber that calls compateTo and other";
+  }
+
+  public String conpareTo(RealNumber irrational, RationalNumber rational){
+    return "the return statement will compare the RationalNumber that calls compateTo and other";
+  }
+
 }
--- RealNumber.java
+++ RealNumber.java
@@ -1,3 +1,25 @@
 public class RealNumber{
-  
+  private double x;
+
+  public RealNumber(){
+    x = 10;
+  }
+
+  public RealNumber(double value){
+    x = value;
+  }
+
+  public double getNum(){
+    return x;
+  }
+
+  public void setNum(int val){
+    x = val;
+  }
+
+  public String toString(){
+    return "";
+  }
+
+
 }
